e3fbb7ae016c2a21645787f3567849ce815c7800
Omar <omarnabilhawwash@gmail.com>
2017-10-23 13:22:27 +0200

    test
    test

---
+++ src/sql/SQLConnect.java
@@ -0,0 +1,12 @@
+package sql;
+
+/**
+ * Created by omnhaww on 23/10/2017.
+ */
+public class SQLConnect {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+    }
+}
