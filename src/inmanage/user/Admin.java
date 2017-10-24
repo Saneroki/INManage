f84162c84742f1b9e0c37c6ff34bcf0d9a8d2274
Pepak16 <Pepak16@student.sdu.dk>
2017-10-24 02:07:01 +0200

    Første ændringer af mig
    Har sorteret i packages og ændret navnene af klasserne som Omar havde lavet. Tilføjet nye klasser.

---
+++ src/inmanage/user/Admin.java
@@ -0,0 +1,12 @@
+package inmanage.user;
+
+public class Admin extends User {
+    public Admin(String firstname, String lastname, String username, String password, String usertype) {
+        super(firstname, lastname, username, password, usertype);
+    }
+
+    /**
+     *  You know any attributes and methods that a admin can use? Write it down please.
+     */
+
+}
--- src/sql/SQLCommands.java
+++ src/sql/SQLCommands.java
@@ -2,20 +2,23 @@ package sql;
 
 import java.sql.Connection;
 
-import inmanagepkg.ProgramImpl;
-import inmanagepkg.IProgram;
+import inmanage.Program;
+import inmanage.IProgram;
 
 /**
  * Created by omnhaww on 23/10/2017.
  */
 public class SQLCommands {
     IProgram pri;
+    SQLConnect SQL_connect;
+    Connection c;
 
-    SQLConnect SQL_connect = new SQLConnect();
-    Connection s = SQL_connect.connect();
-
+    public SQLCommands() {
+        c = SQL_connect.connect();
+        SQL_connect = new SQLConnect();
+    }
 
     private IProgram createProgInterface() {
-        return ProgramImpl.getProgramInstance();
+        return Program.getProgramInstance();
     }
 }
