30dcd81bf549eeb735202fbd5e9fc3c5b5772062
Omar <omarnabilhawwash@gmail.com>
2017-10-24 23:24:01 +0200

    Working code
    After issues with merging branches and what-not, and my commits somehow
    disappearing despite having been pushed earlier today, here’s what
    should contain everything…

--- src/sql/SQLConnect.java
+++ src/sql/SQLConnect.java
@@ -7,9 +7,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Created by omnhaww on 23/10/2017.
+ * Created by omhaw16 on 23/10/2017.
  */
 public class SQLConnect {
+
+    // Main method, mainly for testing purposes. /omhaw16
 
     public static void main(String[] args) throws SQLException {
 
@@ -17,6 +19,10 @@ public class SQLConnect {
         sql.connect();
 
     }
+
+    /* The method used to connect. This will replace the main method, once sufficient testing
+    * has been done. /omhaw16
+    */
         public Connection connect() throws SQLException {
             String url = "jdbc:postgresql://tek-mmmi-db0a.tek.c.sdu.dk/si3_2017_group_1_db";
             String user = "si3_2017_group_1";
@@ -24,8 +30,8 @@ public class SQLConnect {
             Connection db = DriverManager.getConnection(url, user, password);
             try {
                 db = DriverManager.getConnection(url, user, password);
-
                 System.out.println("Connection to database successful!");
+                System.out.println("Welcome!");
 
             } catch (SQLException ex) {
                 Logger lgr = Logger.getLogger(SQLConnect.class.getName());
