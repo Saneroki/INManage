c81432c566aece2bc01efd2cdd81ecd9532b4d47
Omar <omarnabilhawwash@gmail.com>
2017-10-25 09:27:17 +0200

    Renamed Program to ProgramImpl
    Just to keep it organized ;)

--- src/domain/ProgramImpl.java
+++ src/domain/ProgramImpl.java
@@ -5,14 +5,14 @@ import sql.SQLCommands;
 /**
  * Created by omnhaww on 23/10/2017.
  */
-public class Program implements IProgram {
+public class ProgramImpl implements IProgram {
 
     private SQLCommands sql;
 
     private static Program instance = null;
 
 
-    public Program() {
+    public ProgramImpl() {
 
         sql = new SQLCommands();
 
