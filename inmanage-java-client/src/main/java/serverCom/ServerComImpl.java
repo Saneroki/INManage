0ad6cdfd83fc575097731a501c500251b687e1b0
SubotaiKhan <Malteomandersen@gmail.com>
2017-11-09 12:31:14 +0100

    servercom more or less done on client side
    Serverside need to be implementet for this to work

--- inmanage-java-client/src/main/java/serverCom/ServerComImpl.java
+++ inmanage-java-client/src/main/java/serverCom/ServerComImpl.java
@@ -3,7 +3,12 @@ package main.java.serverCom;
 import gen.java.AdminsApi;
 import gen.java.api.UserApi;
 import gen.java.invoker.ApiException;
+import gen.java.model.AddTaskObject;
+import gen.java.model.Project;
+import gen.java.model.Task;
 import gen.java.model.User;
+
+import java.util.List;
 
 public class ServerComImpl implements ServerCom {
 
@@ -34,6 +39,47 @@ public class ServerComImpl implements ServerCom {
             e.printStackTrace();
         }
 
+        return null;
+    }
+
+    @Override
+    public void addProject(Project project) {
+        try {
+            userApi.addProject(project);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void addTask(AddTaskObject task) {
+        try {
+            userApi.addTask(task);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Project> getAllProjects(String userID) {
+
+        try {
+            return userApi.getAllProjects(userID);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllTasks(String projectID) {
+        try {
+            return userApi.getTask(projectID);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
