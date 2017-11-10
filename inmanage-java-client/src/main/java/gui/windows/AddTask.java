0ebca56cbf0d2b22ff544f5c7b4faceb22c59f6c
Omar <omarnabilhawwash@gmail.com>
2017-11-10 19:41:27 +0100

    Add task added to GUI. Implementation missing, I'll do it in the train. /omhaw16

---
+++ inmanage-java-client/src/main/java/gui/windows/AddTask.java
@@ -0,0 +1,47 @@
+package main.java.gui.windows;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+/**
+ * Created by omnhaww on 10/11/2017.
+ */
+
+public class AddTask {
+
+    @FXML
+    private TextField addTaskNameField;
+
+    @FXML
+    private TextArea addTaskDescField;
+
+    @FXML
+    private Button addProjConfirmBtn;
+
+    @FXML
+    private Button addProjCancelBtn;
+
+    @FXML
+    private DatePicker dueDateField;
+
+    @FXML
+    void addProjectCancelButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addProjectConfirmAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dueDateAction(ActionEvent event) {
+
+    }
+
+}
