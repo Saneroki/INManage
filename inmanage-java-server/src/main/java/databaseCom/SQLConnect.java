dbdae19078317c3c51e7fe25456c5254b20623f6
Pepak16 <Pepak16@student.sdu.dk>
2017-11-10 10:26:48 +0100

    Deleted domain package (not necessary for the project), Merged with Omars Task methods in SQLCommands

--- inmanage-java-server/src/main/java/databaseCom/SQLConnect.java
+++ inmanage-java-server/src/main/java/databaseCom/SQLConnect.java
@@ -6,32 +6,28 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Created by omhaw16 on 23/10/2017.
- */
 public class SQLConnect {
 
-        /**
-         * omhaw16
-         * @return Connection
-         * @throws SQLException
-         *
-         * The method used to connect.
-         */
-        public Connection connect() throws SQLException {
-            System.out.println("well hello there!");
-            String url = "jdbc:postgresql://tek-mmmi-db0a.tek.c.sdu.dk/si3_2017_group_1_db";
-            String user = "si3_2017_group_1";
-            String password = "Mach901&blow";
-            Connection db = DriverManager.getConnection(url, user, password);
-            try {
-                db = DriverManager.getConnection(url, user, password);
-                System.out.println("Connection to database successful!");
-                System.out.println("Welcome!");
-            } catch (SQLException ex) {
-                Logger lgr = Logger.getLogger(SQLConnect.class.getName());
-                lgr.log(Level.WARNING, ex.getMessage(), ex);
-            }
-            return db;
+    /**
+     * Made by omhaw16.
+     * The method used to connect.
+     * @return Connection
+     * @throws SQLException
+     */
+    public Connection connect() throws SQLException {
+        System.out.println("well hello there!");
+        String url = "jdbc:postgresql://tek-mmmi-db0a.tek.c.sdu.dk/si3_2017_group_1_db";
+        String user = "si3_2017_group_1";
+        String password = "Mach901&blow";
+        Connection db = DriverManager.getConnection(url, user, password);
+        try {
+            db = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to database successful!");
+            System.out.println("Welcome!");
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(SQLConnect.class.getName());
+            lgr.log(Level.WARNING, ex.getMessage(), ex);
         }
+        return db;
     }
+}
