ffda7a5a0369f1ae331f59df7700c07794e842b7
Omar <omarnabilhawwash@gmail.com>
2017-11-10 20:58:31 +0100

    Add task GUI is completely done now. Revision idea is in a comment in the actual file. /omhaw16

--- inmanage-java-client/src/main/java/gui/windows/AddTask.java
+++ inmanage-java-client/src/main/java/gui/windows/AddTask.java
@@ -7,6 +7,8 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.time.LocalDate;
+
 
 /**
  * Created by omnhaww on 10/11/2017.
@@ -21,27 +23,33 @@ public class AddTask {
     private TextArea addTaskDescField;
 
     @FXML
-    private Button addProjConfirmBtn;
+    private Button addTaskConfirmBtn;
 
     @FXML
-    private Button addProjCancelBtn;
+    private Button addTaskCancelBtn;
 
     @FXML
     private DatePicker dueDateField;
 
-    @FXML
-    void addProjectCancelButton(ActionEvent event) {
+    private String taskName;
 
+    private String taskDesc;
+
+    private LocalDate taskDueDate;
+
+    @FXML
+    void addTaskCancelButton(ActionEvent event) {
+        System.exit(1);
     }
 
     @FXML
-    void addProjectConfirmAction(ActionEvent event) {
+    void addTaskConfirmAction(ActionEvent event) {
+        taskName = addTaskNameField.getText();
+        taskDesc = addTaskDescField.getText();
+        taskDueDate = dueDateField.getValue();
 
-    }
-
-    @FXML
-    void dueDateAction(ActionEvent event) {
-
+        // When implementing this with SQL we have to check whether LocalDate can be used, or whether we need
+        // another solution. /omhaw16
     }
 
 }
