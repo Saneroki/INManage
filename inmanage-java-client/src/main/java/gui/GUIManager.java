ba022eb43376e94737b4444134d053cd33254499
Glenn Drescher <GlennD@me.com>
2017-12-04 04:12:28 +0100

    GUIManager now searches for all fxml files and creates Hashmap for layouts and components.
    So if you want to change a layout to fx. login you simply write:
    GuiManager.setLayout("Login");

--- inmanage-java-client/src/main/java/gui/GUIManager.java
+++ inmanage-java-client/src/main/java/gui/GUIManager.java
@@ -31,7 +31,6 @@ public class GUIManager {
 
     public void setLayout(String fxml) {
         Stage stage = ClientLauncher.getPrimaryStage();
-        System.out.println(layouts.get(fxml));
         Parent position = getFXMLParent(layouts.get(fxml));
         stage.setTitle("INManage");
         stage.setScene(new Scene(position));
@@ -47,15 +46,15 @@ public class GUIManager {
     private Parent getFXMLParent(String fxml) {
         try {
             URL fxmlUrl = Paths.get(fxml).toUri().toURL();
-            Parent root = new FXMLLoader().load(fxmlUrl);
-            return root;
+            new FXMLLoader();
+            return FXMLLoader.load(fxmlUrl);
         } catch (IOException e) {
             throw new Error("The file " + fxml + " was not found");
         }
     }
 
     /**
-     * Creates a hashmap for all the files in the param directory with its name as key and path as value.
+     * Creates a hashmap for all the files in the param directory with its name as key and absolute path as value.
      * Used by the constructor
      *
      * @param path the path to the desired resource folder
@@ -64,19 +63,21 @@ public class GUIManager {
      */
     private HashMap<String, String> getFxmlRes(String path) {
         HashMap<String, String> hashmap = new HashMap<>();
-        File folder = null;
         try {
-            folder = new File(getClass().getClassLoader().getResource(path).toURI());
+            File folder = new File(getClass().getClassLoader().getResource(path).toURI());
+            for (final File file : folder.listFiles()) {
+                if (file.getName().endsWith(".fxml")) {
+                    // Removes the FXML extension from the filename
+                    String fileName = file.getName().substring(0, file.getName().length() - 5);
+                    hashmap.put(fileName, file.getPath());
+                }
+            }
         } catch (URISyntaxException e) {
             throw new Error("Unable to convert the Folder path into a URI");
+        } catch (NullPointerException e) {
+            throw new Error("Unable to find the resource folder or file");
         }
-        for (final File file : folder.listFiles()) {
-            if (file.getName().endsWith(".fxml")) {
-                /** Removes the FXML extension from the filename */
-                String fileName = file.getName().substring(0, file.getName().length() - 5);
-                hashmap.put(fileName, file.getPath());
-            }
-        }
+
         return hashmap;
     }
 }
