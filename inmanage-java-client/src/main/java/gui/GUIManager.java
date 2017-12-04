cd0db34e6d111a7bb4566c2504ff74789f8d1925
Glenn Drescher <GlennD@me.com>
2017-12-04 03:09:11 +0100

    GUIManager now searches for all fxml files and creates Hashmap for layouts and components.
    So if you want to change a layout to fx. login you simply write:
    GuiManager.setLayout("Login");

--- inmanage-java-client/src/main/java/gui/GUIManager.java
+++ inmanage-java-client/src/main/java/gui/GUIManager.java
@@ -5,38 +5,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * This Class does all the work for changing scenes and content of them
  */
 public class GUIManager {
     private FXMLLoader loader;
+    private HashMap<String, String> layouts;
+    private HashMap<String, String> components;
 
     /**
-     * Searches for all the fxml files and creates a map with its name and path
+     * Loads all the correct fxml file paths and makes them available via the filename
+     *
      * @author Glenn
      */
-    public GUIManager(){
-        HashMap<String, String> layouts = createFXMLFileMap("fxml/layouts");
-        HashMap<String, String> components = createFXMLFileMap("fxml/components");
-        System.out.println(layouts.get("Login"));
+    public GUIManager() {
+        layouts = getFxmlRes("fxml/layouts");
+        components = getFxmlRes("fxml/components");
     }
 
-
-
-    public void setScene(String window){
+    public void setLayout(String layout) {
         Stage stage = ClientLauncher.getPrimaryStage();
         Parent position;
-        setFxmlLoader(window);
+        setFxmlLoader(layout);
         try {
             position = loader.load();
         } catch (IOException e) {
-            throw new Error("The file for " + window + " could not be found!");
+            throw new Error("The file for " + layout + " could not be found!");
         }
         stage.setTitle("INManage");
         stage.setScene(new Scene(position));
@@ -46,50 +47,36 @@ public class GUIManager {
     /**
      * Sets the fxml loader depending on the requested path
      * Used by setScene
+     *
      * @param fxmlPath
      */
-    private void setFxmlLoader(String fxmlPath){
+    private void setFxmlLoader(String fxmlPath) {
         loader = new FXMLLoader(getClass().getClassLoader().getResource(fxmlPath));
     }
 
     /**
      * Creates a hashmap for all the files in the param directory with its name as key and path as value.
      * Used by the constructor
-     * @author Glenn
+     *
      * @param path the path to the desired resource folder
      * @return a Hashmap with all the files in the param directory
+     * @author Glenn
      */
-    private HashMap createFXMLFileMap(String path) {
+    private HashMap<String, String> getFxmlRes(String path) {
         HashMap<String, String> hashmap = new HashMap<>();
-        ClassLoader classLoader = getClass().getClassLoader();
-        File folder = new File(classLoader.getResource(path).getFile());
-        File[] listOfFiles = folder.listFiles((dir, name) -> name.endsWith(".fxml"));
-
-        for (File listOfFile : listOfFiles) {
-            String filename = listOfFile.getName();
-            filename = filename.substring(0, filename.length() - 5);
-            hashmap.put(filename, listOfFile.getPath());
+        File folder = null;
+        try {
+            folder = new File(getClass().getClassLoader().getResource(path).toURI());
+        } catch (URISyntaxException e) {
+            throw new Error("Unable to convert the Folder path into a URI");
+        }
+        for (final File file : folder.listFiles()) {
+            if (file.getName().endsWith(".fxml")) {
+                /** Removes the FXML extension from the filename */
+                String fileName = file.getName().substring(0, file.getName().length() - 5);
+                hashmap.put(fileName, file.getPath());
+            }
         }
         return hashmap;
-    }
-
-    /**
-     * Get files or directories from the resource folder.
-     * This was developed because a normal string as path gave a lot of errors.
-     * @author Glenn
-     * @param resFolder path to a desired resource folder.
-     * @return the path, which java can read
-     */
-    private File getFileFromURL(String resFolder) {
-        URL url = this.getClass().getClassLoader().getResource(resFolder);
-        File file = null;
-        try{
-            file = new File(url.toURI());
-        } catch (URISyntaxException e) {
-            url.getPath();
-            file = new File(url.getPath());
-        } finally {
-            return file;
-        }
     }
 }
