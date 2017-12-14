b8390ca4aae2215ae693e912611005b3987879d1
Omar <omarnabilhawwash@gmail.com>
2017-12-15 00:38:56 +0100

    Edit GUIs done

---
+++ inmanage-java-client/src/main/java/gui/layouts/EditProject.java
@@ -0,0 +1,14 @@
+package main.java.gui.layouts;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by omnhaww on 14/12/2017.
+ */
+public class EditProject {
+    public void editProjectConfirmAction(ActionEvent actionEvent) {
+    }
+
+    public void editProjectCancelButton(ActionEvent actionEvent) {
+    }
+}
