8dccf01a6a6904b05e50a7f74578210f64fed4ed
Omar <omarnabilhawwash@gmail.com>
2017-12-17 17:20:09 +0100

    Sign out dialog box DONE. Just needs implementation on the topbar, and we're good

--- inmanage-java-client/src/main/java/gui/layouts/ProjectDashboard.java
+++ inmanage-java-client/src/main/java/gui/layouts/ProjectDashboard.java
@@ -7,9 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -33,6 +31,8 @@ public class ProjectDashboard extends Controller {
     @FXML
     private Label welcomeText;
 
+    private Alert alert;
+
 
     private final ArrayList<Project> list =
             (ArrayList<Project>) ClientLauncher.getServer().getAllProjects(ClientLauncher.getUserID());
@@ -41,20 +41,19 @@ public class ProjectDashboard extends Controller {
     private ServerCom serv = ClientLauncher.getServer();
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         vboxLeft.setSpacing(15);
         vboxLeft.setMinWidth(250);
         vboxLeft.setMaxHeight(250);
 
-        list.parallelStream().forEach(proj -> new ProjectView(proj,serv.getUserAmount(proj.getId()),serv.getTaskAmount(proj.getId()),vboxLeft));
-
+        list.parallelStream().forEach(proj -> new ProjectView(proj, serv.getUserAmount(proj.getId()), serv.getTaskAmount(proj.getId()), vboxLeft));
 
 
         addNewProjectBtn.setOnAction(event -> ClientLauncher.getWindowChanger().setLayout("AddProject"));
 
         editUserBtn.setOnAction(event -> ClientLauncher.getWindowChanger().setLayout("EditUser"));
 
-        welcomeText.setText("Welcome "+ClientLauncher.getUser().getName());
+        welcomeText.setText("Welcome " + ClientLauncher.getUser().getName());
 
     }
 
@@ -74,8 +73,7 @@ public class ProjectDashboard extends Controller {
     }
 
 
-
-    private void populateListView(){
+    private void populateListView() {
         //list = serv.getAllProjects(ClientLauncher.getUserID());
 
         //projectList = new ListView<Project>();
@@ -88,11 +86,22 @@ public class ProjectDashboard extends Controller {
 
     @FXML
     public void SignOut(ActionEvent actionEvent) {
-        ClientLauncher.setUserId("");
-        ClientLauncher.getUser().setName("");
-        ClientLauncher.getUser().setPassword("");
-        welcomeText.setText("");
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
 
-        ClientLauncher.getWindowChanger().setLayout("Login");
+        alert.setTitle("Sign out?");
+        alert.setContentText("Are you sure you'd like to sign out?");
+
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.OK) {
+            ClientLauncher.setUserId("");
+            ClientLauncher.getUser().setName("");
+            ClientLauncher.getUser().setPassword("");
+            welcomeText.setText("");
+            ClientLauncher.getWindowChanger().setLayout("Login");
+
+        } if (alert.getResult() == ButtonType.CANCEL) {
+            alert.close();
+        }
     }
 }
